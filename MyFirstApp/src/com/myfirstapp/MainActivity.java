07ba57208043c0c0f122ee1d7bce15bd646467cc
Marina Melo <mmelo@globosat.com.br>
2012-08-29 14:34:26 -0300

    incluindo o src

---
+++ MyFirstApp/src/com/myfirstapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.myfirstapp.MESSAGE";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+    
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
+    
+}
